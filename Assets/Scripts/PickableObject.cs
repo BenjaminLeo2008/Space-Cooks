5f1d77e443b1bfff3be87d8503d059a21fb90aa3
BenjaminLeo2008 <166539016+BenjaminLeo2008@users.noreply.github.com>
2025-08-18 14:27:20 -0300

    Pick0.5

---
+++ Assets/Scripts/PickableObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableObject : MonoBehaviour
+{
+    public bool IsPickable = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PlayerInteractionZone")
+        {
+            other.GetComponentInParent<PickUpObject>().ObjectToPickUp = this.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "PlayerInteractionZone")
+        {
+            other.GetComponentInParent<PickUpObject>().ObjectToPickUp = null;
+        }
+    }
+}
