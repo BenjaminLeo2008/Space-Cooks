df9a18528bdb275718296690ff2c60548967b6e7
LolaDoming <codasdom10@gmail.com>
2025-09-17 14:31:40 -0300

    BarraTiempoArreglo

--- Assets/Scripts/ContadorTiempo.cs
+++ Assets/Scripts/ContadorTiempo.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ContadorTiempo : MonoBehaviour
 {
+    public GameObject TiempoUI;
+    public PlayerController player;
+    public string escenaPuntaje = "EscenaPuntaje";
     public int min, seg;
     public TextMeshProUGUI TimerTxt;
 
@@ -26,7 +30,7 @@ public class ContadorTiempo : MonoBehaviour
             if (restante > 0)
             {
                 restante -= Time.deltaTime;
-            if (restante < 0)
+                if (restante < 0)
                 {
                     restante = 0;
                 }
@@ -38,7 +42,18 @@ public class ContadorTiempo : MonoBehaviour
             {
                 restante = 0;
                 enMarcha = false;
+                StartCoroutine(FinDelTiempo());
             }
         }
+    }
+    IEnumerator FinDelTiempo()
+    {
+        TiempoUI.SetActive(true);
+        if (player != null)
+        {
+            player.enabled = false;
+        }
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(escenaPuntaje);
     }
 }
--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -6,18 +6,14 @@ using UnityEngine.SceneManagement;
 
 public class TimerController : MonoBehaviour
 {
-    public GameObject TiempoUI;
     public Image timer_linear_image;
 
     public float tiempoInicial = 60f;
     private float tiempoRestante;
-    public PlayerController player;
-    public string escenaPuntaje = "EscenaPuntaje";
 
     void Start()
     {
         tiempoRestante = tiempoInicial;
-        TiempoUI.SetActive(false);
     }
 
     // Update is called once per frame
@@ -30,19 +26,8 @@ public class TimerController : MonoBehaviour
             if (tiempoRestante <= 0)
             {
                 tiempoRestante = 0;
-                StartCoroutine(FinDelTiempo());
             }
         }
 
-    }
-    IEnumerator FinDelTiempo()
-    {
-        TiempoUI.SetActive(true);
-        if (player != null)
-        {
-            player.enabled = false;
-        }
-        yield return new WaitForSeconds(2f);
-        SceneManager.LoadScene(escenaPuntaje);
     }
 }
