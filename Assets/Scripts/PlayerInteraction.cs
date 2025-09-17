d389a6411614b6a873272a9a06f230846535549f
BenjaminLeo2008 <166539016+BenjaminLeo2008@users.noreply.github.com>
2025-09-16 23:55:53 -0300

    PlateFixxed

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerInteraction : MonoBehaviour
 {
     [SerializeField] public ObjectCatcherScript objectCatcher;
-    
+
     public GameObject ObjectToPickUp;
     public GameObject PickedObject;
     public Transform PlayerInteractionZone;
@@ -15,6 +15,10 @@ public class PlayerInteraction : MonoBehaviour
     private List<GameObject> _tileCloseList = new List<GameObject>();
     private GameObject _closestTile;
     private Vector3 originalScale;
+
+    // Nueva variable para guardar la referencia al script del objeto.
+    private ObjectCatcherScript _caughtCatcherScript;
+    private SphereCollider _caughtCol;
 
     void Start()
     {
@@ -33,5 +37,59 @@ public class PlayerInteraction : MonoBehaviour
 
     void Update()
     {
+        if (ObjectToPickUp != null && ObjectToPickUp.GetComponent<PickableObject>().IsPickable == true && PickedObject == null)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                PickedObject = ObjectToPickUp;
+                PickedObject.GetComponent<PickableObject>().IsPickable = false;
+
+                // Intentamos obtener el script ObjectCatcherScript del objeto.
+                _caughtCatcherScript = PickedObject.GetComponent<ObjectCatcherScript>();
+                if (_caughtCatcherScript != null)
+                {
+                    // Si el script existe, lo desactivamos.
+                    _caughtCatcherScript.enabled = false;
+                }
+                _caughtCol = PickedObject.GetComponent<SphereCollider>();
+                if (_caughtCol != null)
+                {
+                    // Si el script existe, lo desactivamos.
+                    _caughtCol.enabled = false;
+                }
+                originalScale = PickedObject.transform.localScale;
+
+                PickedObject.transform.SetParent(PlayerInteractionZone);
+                PickedObject.transform.localPosition = Vector3.zero;
+                PickedObject.transform.localRotation = Quaternion.identity;
+                PickedObject.transform.localScale = originalScale; // Establece la escala local del objeto recogido
+                PickedObject.GetComponent<Rigidbody>().useGravity = false;
+                PickedObject.GetComponent<Rigidbody>().isKinematic = true;
+            }
+        }
+        else if (PickedObject != null)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                PickedObject.GetComponent<PickableObject>().IsPickable = true;
+                PickedObject.transform.SetParent(null);
+                PickedObject.GetComponent<Rigidbody>().useGravity = true;
+                PickedObject.GetComponent<Rigidbody>().isKinematic = false;
+
+
+                PickedObject.transform.localScale = originalScale;
+
+                // Si teníamos un script guardado, lo volvemos a activar.
+                if (_caughtCatcherScript != null)
+                {
+                    _caughtCatcherScript.enabled = true;
+                }
+                if (_caughtCol != null)
+                {
+                    _caughtCol.enabled = true;
+                }
+                PickedObject = null;
+            }
+        }
     }
 }
--- Assets/Scripts/Utils/ObjectCatcherScript.cs
+++ Assets/Scripts/Utils/ObjectCatcherScript.cs
@@ -19,50 +19,8 @@ public class ObjectCatcherScript : MonoBehaviour
     public Transform interactionZone;
     private Vector3 originalScale;
 
-    public Transform PlayerInteractionZone;
-
     // Propiedad pública para que otros scripts puedan leer el objeto atrapado.
     public GameObject PickedObject => _pickedObject;
-
-    public void HandlePickup()
-    {
-        if (pickedObject == null)
-        {
-            Collider[] hitColliders = Physics.OverlapSphere(transform.position, detectionRadius);
-            foreach (var hitCollider in hitColliders)
-            {
-                if (hitCollider.CompareTag("Object") || hitCollider.CompareTag("Plate"))
-                {
-                    PickableObject objectToPickup = hitCollider.GetComponent<PickableObject>();
-                    if (objectToPickup != null && objectToPickup.IsPickable)
-                    {
-                        _pickedObject = hitCollider.gameObject;
-                        objectToPickup.IsPickable = false;
-
-                        originalScale = _pickedObject.transform.localScale;
-                        _pickedObject.transform.SetParent(interactionZone);
-
-                        _pickedObject.transform.localPosition = Vector3.zero;
-                        _pickedObject.transform.localRotation = Quaternion.identity;
-                        _pickedObject.GetComponent<Rigidbody>().useGravity = false;
-                        _pickedObject.GetComponent<Rigidbody>().isKinematic = true;
-                        return;
-
-                    }
-                }
-            }
-        }
-        else
-        {
-            _pickedObject.transform.SetParent(null);
-            _pickedObject.GetComponent<Rigidbody>().isKinematic = false;
-            _pickedObject.GetComponent<Rigidbody>().useGravity = true;
-            _pickedObject.transform.localScale = originalScale;
-
-            _pickedObject.GetComponent<PickableObject>().IsPickable = true;
-            _pickedObject = null;
-        }
-    }
 
     // Nuevo método público para que otros scripts puedan establecer el objeto atrapado.
     public void SetPickedObject(GameObject newObject)
@@ -132,89 +90,74 @@ public class ObjectCatcherScript : MonoBehaviour
                     }
                 }
             }
-            else if (gameObject.CompareTag("Plate"))
+        }
+        else if (gameObject.CompareTag("Plate"))
+        {
+            // Solo ejecuta la lógica si un objeto ha sido atrapado
+            if (_pickedObject != null)
             {
-                // Solo ejecuta la lógica si un objeto ha sido atrapado
-                if (_pickedObject != null)
+                // Opcional: Asegúrate de que el objeto no tiene un padre (esto ayuda a evitar problemas)
+                if (_pickedObject.transform.parent == null)
                 {
-                    // Opcional: Asegúrate de que el objeto no tiene un padre (esto ayuda a evitar problemas)
-                    if (_pickedObject.transform.parent == null)
+                    // Verifica que la superficie exista y el objeto tenga el tag correcto
+                    if (_superficieTransform != null && _pickedObject.CompareTag("Object") || _pickedObject.CompareTag("Plate"))
                     {
-                        // Verifica que la superficie exista y el objeto tenga el tag correcto
-                        if (_superficieTransform != null && _pickedObject.CompareTag("Object") || _pickedObject.CompareTag("Plate"))
-                        {
-                            // Obtiene la altura del objeto con su collider
-                            float objectHeight = _pickedObject.GetComponent<Collider>().bounds.extents.y;
-                            Vector3 superficiePosition = _superficieTransform.position;
+                        // Obtiene la altura del objeto con su collider
+                        float objectHeight = _pickedObject.GetComponent<Collider>().bounds.extents.y;
+                        Vector3 superficiePosition = _superficieTransform.position;
 
-                            // Calcula la nueva posición del objeto
-                            Vector3 newPosition = new Vector3(
-                                superficiePosition.x + offset.x,
-                                superficiePosition.y + objectHeight + offset.y,
-                                superficiePosition.z + offset.z
-                            );
+                        // Calcula la nueva posición del objeto
+                        Vector3 newPosition = new Vector3(
+                            superficiePosition.x + offset.x,
+                            superficiePosition.y + objectHeight + offset.y,
+                            superficiePosition.z + offset.z
+                        );
 
-                            // Establece la posición y rotación del objeto
-                            _pickedObject.transform.position = newPosition;
-                            _pickedObject.transform.rotation = _superficieTransform.rotation;
-                            originalScale = _pickedObject.transform.localScale;
-                            // Una vez posicionado, establece el padre
-                            _pickedObject.transform.SetParent(interactionZone);
-                        }
+                        // Establece la posición y rotación del objeto
+                        _pickedObject.transform.position = newPosition;
+                        _pickedObject.transform.rotation = _superficieTransform.rotation;
+                        originalScale = _pickedObject.transform.localScale;
+                        // Una vez posicionado, establece el padre
+                        _pickedObject.transform.SetParent(interactionZone);
                     }
                 }
             }
-            else if (PickedObject != null)
+        }
+    }
+
+    // Este método se activa cuando un Collider entra en el trigger
+    void OnTriggerEnter(Collider other)
+    {
+        // Detecta el objeto solo si no estamos sosteniendo uno y si tiene el tag correcto
+        if (_pickedObject == null && other.gameObject.CompareTag("Object") || other.gameObject.CompareTag("Plate"))
+        {
+            // Obtiene el Rigidbody del objeto que entró
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            if (rb != null)
             {
-                if (Input.GetKeyDown(KeyCode.F))
-                {
-                    PickedObject.GetComponent<PickableObject>().IsPickable = true;
-                    PickedObject.transform.SetParent(null);
-                    PickedObject.GetComponent<Rigidbody>().useGravity = true;
-                    PickedObject.GetComponent<Rigidbody>().isKinematic = false;
-
-
-                    PickedObject.transform.localScale = originalScale;
-
-                    _pickedObject = null;
-                }
+                // Si el objeto tiene un Rigidbody, lo "atrapamos"
+                _pickedObject = other.gameObject;
+                _caughtRigidbody = rb;
+                // Lo hacemos cinemático para que no sea afectado por las físicas
+                _caughtRigidbody.isKinematic = true;
             }
         }
+    }
 
-        // Este método se activa cuando un Collider entra en el trigger
-        void OnTriggerEnter(Collider other)
+    // Este método se activa cuando un Collider sale del trigger
+    void OnTriggerExit(Collider other)
+    {
+        // Si el objeto que sale es el que teníamos atrapado
+        if (other.gameObject == _pickedObject)
         {
-            // Detecta el objeto solo si no estamos sosteniendo uno y si tiene el tag correcto
-            if (_pickedObject == null && other.gameObject.CompareTag("Object") || other.gameObject.CompareTag("Plate"))
+            // Si el Rigidbody existe, lo volvemos a un estado no cinemático
+            if (_caughtRigidbody != null)
             {
-                // Obtiene el Rigidbody del objeto que entró
-                Rigidbody rb = other.GetComponent<Rigidbody>();
-                if (rb != null)
-                {
-                    // Si el objeto tiene un Rigidbody, lo "atrapamos"
-                    _pickedObject = other.gameObject;
-                    _caughtRigidbody = rb;
-                    // Lo hacemos cinemático para que no sea afectado por las físicas
-                    _caughtRigidbody.isKinematic = true;
-                }
+                _caughtRigidbody.isKinematic = false;
             }
-        }
-
-        // Este método se activa cuando un Collider sale del trigger
-        void OnTriggerExit(Collider other)
-        {
-            // Si el objeto que sale es el que teníamos atrapado
-            if (other.gameObject == _pickedObject)
-            {
-                // Si el Rigidbody existe, lo volvemos a un estado no cinemático
-                if (_caughtRigidbody != null)
-                {
-                    _caughtRigidbody.isKinematic = false;
-                }
-                // Limpiamos las referencias
-                _pickedObject = null;
-                _caughtRigidbody = null;
-            }
+            // Limpiamos las referencias
+            _pickedObject = null;
+            _caughtRigidbody = null;
         }
     }
 }
