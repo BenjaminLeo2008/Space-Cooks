fef5521c6d096e3a82ff21c05ec7be75b3f27c2c
BenjaminLeo2008 <166539016+BenjaminLeo2008@users.noreply.github.com>
2025-10-27 14:25:42 -0300

    👎

---
+++ Assets/Scripts/ComplexFoodSO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[CreateAssetMenu("ComplexFoodSO")]
+public class ComplexFoodSO : ScriptableObject
+{
+    public GameObject complexFoodPrefab;
+    public IngredientData[] ingredientsNeededForDish;
+}
