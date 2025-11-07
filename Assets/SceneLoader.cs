88fe259a9b7a6f42174ade80400cc1d0a29be265
LolaDoming <codasdom10@gmail.com>
2025-11-07 09:57:01 -0300

    push

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{ // Hola codis
+    public GameObject loaderUI;
+    public Image BarraAzul;
+
+    public void LoadScene(int index)
+    {
+        StartCoroutine(LoadScene_Coroutine(index));
+    }
+    public IEnumerator LoadScene_Coroutine(int index)
+    {
+        BarraAzul.fillAmount = 0f;
+        loaderUI.SetActive(true);
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(index);
+        asyncOperation.allowSceneActivation = false;
+        float progress = 0f;
+
+        while (asyncOperation.isDone)
+        {
+            progress = Mathf.MoveTowards(progress, asyncOperation.progress, Time.deltaTime);
+            BarraAzul.fillAmount = progress;
+            if (progress >= 0.9f)
+            {
+                BarraAzul.fillAmount = 1;
+                asyncOperation.allowSceneActivation = true;
+                }
+                yield return null;
+            }
+        }
+    }
+}
--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -2,27 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+//1
 public class TimerController : MonoBehaviour
-{
+{//2
     public Image timer_linear_image;
-
+    //3
     public float tiempoInicialSeg;
     private float tiempoRestante;
-
+    //4
     void Start()
-    {
+    {//5
         tiempoRestante = tiempoInicialSeg;
         if (timer_linear_image == null)
-        {
+        {//6
             Debug.Log("La imagen no esta asignada vro");
-        }
+        }//7
         else
-        {
+        {//8
             timer_linear_image.fillAmount = 1f;
-        }
-    }
-
+        }//9
+    }//10
+    //11
     // Update is called once per frame
     void Update()
     {
@@ -42,3 +42,4 @@ public class TimerController : MonoBehaviour
 
     }
 }
+//1//1
