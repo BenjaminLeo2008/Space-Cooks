291bd257500bc5efff8516f54c28baf4f079fc2d
LolaDoming <codasdom10@gmail.com>
2025-11-13 10:40:57 -0300

    sliderProgreso

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -2,12 +2,29 @@
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
-using TMPro; // Importar si usas TextMeshPro (RECOMENDADO)
 
 public class SceneLoader : MonoBehaviour
 {
     [SerializeField] private Slider loadBar;
     [SerializeField] private GameObject LoadPanel;
+
+    public void SceneLoad(int sceneIndex)
+    {
+        LoadPanel.SetActive(true);
+        StartCoroutine(LoadAsync(sceneIndex));
+    }
+    IEnumerator LoadAsync(int sceneIndex)
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneIndex);
+        while (!asyncOperation.isDone)
+        {
+            Debug.Log(asyncOperation.progress);
+            loadBar.value = asyncOperation.progress;
+            yield return null;
+        }
+
+    }
+
     // link del video: https://youtu.be/cRavxipZQ3s?si=eBl_huWqM4YIhDxK
 }
 
